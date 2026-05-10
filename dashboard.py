ff6dceef98ae64cb46428941f4d9845a8c7f753b
Aaryavshya Fravziyana Vziishyam Sarvashya Onaekas <153837048+Aaryavshya@users.noreply.github.com>
2026-05-10 21:11:43 +0530

    Update dashboard.py: Concentration and TimeStamps added

--- dashboard.py
+++ dashboard.py
@@ -24,13 +24,15 @@ st.write("Web Application Dashboard: SnO2-BN based gas sensing ")
 # Read latest voltage
 latest_ref = db.reference("FireBaseIOT/R")
 
+ref_gas = db.reference("Pred_Gas/Gas")
+ref_concentration = db.reference("Pred_Gas/Concentration")
+
 latest_value = latest_ref.get()
+predicted_gas = ref_gas.get()
+predicted_concentration = ref_concentration.get()
 
-predicted_gas_value = db.reference("Pred_Gas/Gas")
 
-predicted_gas = predicted_gas_value.get()
-
-col1, col2 = st.columns(2)
+col1, col2, col3, col4 = st.columns(4)
 
 if latest_value is not None:
     col1.metric("Current Resistance", latest_value)
@@ -38,9 +40,19 @@ else:
     col1.metric("Current Resistance", "No data")
 
 if predicted_gas is not None:
-    col2.metric("Predicted Gas: ",predicted_gas)
+    col2.metric("Predicted Gas: ", predicted_gas)
 else:
-    col2.metric("Predicted Gas: Processing....")
+    col2.metric("Predicted Gas: ","Processing...")
+
+if predicted_gas is not None:
+    col3.metric("Predicted Gas: ", predicted_gas)
+else:
+    col3.metric("Predicted Gas: ","Processing...")
+
+if predicted_gas is not None:
+    col4.metric("Predicted Gas: ", predicted_gas)
+else:
+    col4.metric("Predicted Gas: ","Processing...")
 
 # Read history
 history_ref = db.reference("FireBaseIOT/Latest")
@@ -63,7 +75,9 @@ if data:
 
     df = df.sort_values("timestamp")
 
-    st.line_chart(df["voltage"],x_label="Sample Points",y_label="Resistance")
+    st.line_chart(df["voltage"],x_label="Sample points",y_label="Resistance")
+
+    # st.write()
 
 else:
     st.write("No history data yet")
