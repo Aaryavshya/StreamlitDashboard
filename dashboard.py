8555f6f3f01cb46ee8c4a48675c969798bc2392d
Aaryavshya Fravziyana Vziishyam Sarvashya Onaekas <153837048+Aaryavshya@users.noreply.github.com>
2026-05-11 10:57:53 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -41,6 +41,8 @@ if session_timestamp is not None:
 else:
     col1.metric("Session Timestamp: ","fetching...")
 
+col2.metric("")
+
 if predicted_gas is not None:
     col1.metric("Predicted Gas: ", predicted_gas)
 else:
